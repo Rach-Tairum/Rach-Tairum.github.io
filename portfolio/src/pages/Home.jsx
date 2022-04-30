b58c48c331dfe70616db3162b6722478832ea574
Rachel Tairum <rachel.tairum@gmail.com>
2022-04-30 08:52:21 -0300

    Desenvolver página de Home e navegação

---
+++ portfolio/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navegation from '../Components/Navegation';
+import { Title, Subtitle } from '../Components/Style'
+
+function Home () {
+  return (
+      <div>
+        <Title>Rachel Tairum</Title>
+        <Subtitle>Portifólio Web</Subtitle>
+        <Navegation />
+      </div>
+    );
+}
+
+export default Home;
